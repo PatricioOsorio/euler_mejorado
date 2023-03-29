f63a8f902fc056317b916240d4add8fdf54a10c6
PatricioOsorio <PatricioMiguel_12@hotmail.com>
2023-03-29 16:45:36 -0600

    add icons, button clear, and colors

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+// Aos library
+document.addEventListener('DOMContentLoaded', (e) => {
+  AOS.init();
+});
+
 const $form = document.querySelector('#myForm');
 const $output = document.querySelector('#output');
 
@@ -5,26 +10,32 @@ $form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // Obtenemos los valores de entrada
-  let yPrimeText = e.target.yPrime.value;
+  let yPrimeText = e.target.yPrime.value.toString();
   let x0 = parseFloat(e.target.x0.value);
   let y0 = parseFloat(e.target.y0.value);
   let yn = parseFloat(e.target.yn.value);
 
+  let y = y0;
+  let x = x0;
+
   const eulerMejorado = (f, x0, y0, yn, n) => {
     let h = (yn - x0) / n;
-    let y = y0;
-    let x = x0;
+
     for (let i = 0; i < n; i++) {
-      let y_pred = y + h * f(x, y);
-      y = y + (h / 2) * (f(x, y) + f(x + h, y_pred));
+      const slope = eval(f.replace(/x/g, x).replace(/y/g, y));
+      const yPrime = y + slope * h;
+      const slopePrime = eval(f.replace(/x/g, x + h).replace(/y/g, yPrime));
+      y = y + ((slope + slopePrime) * h) / 2;
       x = x + h;
     }
     return y;
   };
 
-  const yPrime = (x, y) => eval(yPrimeText);
+  let yr = eulerMejorado(yPrimeText, x0, y0, yn, 1000);
 
-  let y = eulerMejorado(yPrime, x0, y0, yn, 1000);
+  $output.innerHTML = `El valor de y en el punto y<sub>n</sub> es ${yr}`;
+});
 
-  $output.innerHTML = `El valor de y en el punto y<sub>n</sub> es ${y}`;
+$form.addEventListener('reset', (e) => {
+  $output.innerHTML = ``;
 });
