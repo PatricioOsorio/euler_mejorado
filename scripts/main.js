ed5fff1b3738a9579e4aed22eb703f8328d3563b
PatricioOsorio <PatricioMiguel_12@hotmail.com>
2023-03-30 15:08:40 -0600

    Add Math.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,57 +17,67 @@ const Toast = Swal.mixin({
 });
 
 // Validation
-document.addEventListener("input", (e) => {
-  document.querySelector("#myForm").classList.add("was-validated");
+document.addEventListener('input', (e) => {
+  document.querySelector('#myForm').classList.add('was-validated');
 });
-
 
 // My form
 const $form = document.querySelector('#myForm');
 const $output = document.querySelector('#output');
 
+// Listener of reset
 $form.addEventListener('reset', (e) => {
   $output.innerHTML = ``;
 });
 
+// Function of f'
+function dydx(x, y, dydx) {
+  return math.evaluate(dydx, { x: x, y: y });
+}
+
+// Function euler
+function eulerMejorado(f, x0, y0, yn, n) {
+  let y = y0;
+  let x = x0;
+  let h = (yn - x0) / n;
+
+  for (let i = 0; i < n; i++) {
+    k1 = dydx(x, y, f);
+    k2 = dydx(x + h, y + h * k1, f);
+    y = y + (h / 2) * (k1 + k2);
+    x = x + h;
+  }
+  console.log('h: ' + h);
+  return y;
+}
+
+// Listener of form
 $form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // Obtenemos los valores de entrada
-  let yPrimeText = e.target.yPrime.value.toString();
+  let yPrimeText = e.target.yPrime.value;
   let x0 = parseFloat(e.target.x0.value);
   let y0 = parseFloat(e.target.y0.value);
   let yn = parseFloat(e.target.yn.value);
-
-  let y = y0;
-  let x = x0;
-
-  const eulerMejorado = (f, x0, y0, yn, n) => {
-    let h = (yn - x0) / n;
-
-    for (let i = 0; i < n; i++) {
-      const slope = eval(f.replace(/x/g, x).replace(/y/g, y));
-      const yPrime = y + slope * h;
-      const slopePrime = eval(f.replace(/x/g, x + h).replace(/y/g, yPrime));
-      y = y + ((slope + slopePrime) * h) / 2;
-      x = x + h;
-    }
-
-    return y;
-  };
 
   let result;
 
   try {
     result = eulerMejorado(yPrimeText, x0, y0, yn, 1000);
   } catch (error) {
+    // En caso de error se muestra una alerta
     Toast.fire({
       icon: 'error',
       title: 'Ocurrió un error',
     });
   }
- console.log(result);
+  console.log(result);
 
-  $output.innerHTML = `El valor de y en el punto y<sub>${yn}</sub> es: <span class="badge bg-primary-subtle text-primary-emphasis rounded-pill fs-4">${result}</span>`;
+  $output.innerHTML = `
+  El valor de 
+  <span class="badge text-bg-info rounded-pill">y</span> 
+  en el punto 
+  <span class="badge text-bg-success rounded-pill">x<sub>${yn}</sub></span> es: 
+  <p class="badge bg-primary-subtle text-primary-emphasis rounded-pill fs-4">${result}</p>`;
 });
-
